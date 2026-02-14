7c503d928262fe272cb66f6bf73302db59f1dd6a
tnfssc <29162020+tnfssc@users.noreply.github.com>
2026-02-14 11:53:16 +0530

    revert(ui): restore microphone panel icon
    Bring back GNOME's microphone symbolic status icon in the top bar and remove the temporary custom wave icon so panel visuals match previous behavior.

--- extension/extension.js
+++ extension/extension.js
@@ -16,7 +16,6 @@ const DBUS_CONTROL_PATH = '/org/gnome/Shell/Extensions/OpenWispr';
 const COMPANION_BUS_NAME = 'io.github.tnfssc.OpenWispr.Recorder';
 const COMPANION_OBJECT_PATH = '/io/github/tnfssc/OpenWispr/Recorder';
 const COMPANION_INTERFACE = 'io.github.tnfssc.OpenWispr.Recorder';
-const PANEL_ICON_FILENAME = 'openwispr-wave-symbolic.svg';
 const DBUS_CONTROL_IFACE = `
 <node>
   <interface name="org.gnome.Shell.Extensions.OpenWispr">
@@ -72,17 +71,13 @@ class OpenWisprController {
 
         // resolve paths relative to extension dir
         this._modelPath = this.dir.get_child('models').get_child('ggml-base.en.bin').get_path();
-        this._idleIconGicon = null;
-        const panelIconFile = this.dir.get_child(PANEL_ICON_FILENAME);
-        if (panelIconFile.query_exists(null))
-            this._idleIconGicon = new Gio.FileIcon({ file: panelIconFile });
 
         // UI: Panel Indicator
         this._indicator = new PanelMenu.Button(0.0, this.metadata.name, false);
         this._icon = new St.Icon({
+            icon_name: 'microphone-sensitivity-high-symbolic',
             style_class: 'system-status-icon',
         });
-        this._setIdleIcon();
         this._indicator.add_child(this._icon);
         
         // Click to toggle
@@ -220,7 +215,6 @@ class OpenWisprController {
 
         this._dbusConn = null;
         this._companionProxy = null;
-        this._idleIconGicon = null;
 
         Main.wm.removeKeybinding('toggle-recording');
         this._settings = null;
@@ -485,7 +479,6 @@ class OpenWisprController {
         this._debug(`Starting recording (${trigger})...`);
         this._recording = true;
         this._recordingTrigger = trigger;
-        this._icon.gicon = null;
         this._icon.icon_name = 'media-record-symbolic';
         this._icon.style_class = 'system-status-icon destructive-action'; // Red-ish if theme supports
 
@@ -502,7 +495,6 @@ class OpenWisprController {
 
         this._recording = false;
         this._processing = true;
-        this._icon.gicon = null;
         this._icon.icon_name = 'process-working-symbolic'; // Spinner
 
         const proxy = this._getCompanionProxy();
@@ -685,18 +677,8 @@ class OpenWisprController {
         this._processing = false;
         this._recordingTrigger = null;
         this._remoteHoldBinding = null;
-        this._setIdleIcon();
-    }
-
-    _setIdleIcon() {
         if (this._icon) {
-            if (this._idleIconGicon) {
-                this._icon.gicon = this._idleIconGicon;
-                this._icon.icon_name = null;
-            } else {
-                this._icon.gicon = null;
-                this._icon.icon_name = 'microphone-sensitivity-high-symbolic';
-            }
+            this._icon.icon_name = 'microphone-sensitivity-high-symbolic';
             this._icon.style_class = 'system-status-icon';
         }
     }
